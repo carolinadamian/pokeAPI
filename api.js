6e46c5fbc14142956e31e1827f355bbf1b48d22f
carolinadamian <131982806+carolinadamian@users.noreply.github.com>
2023-07-12 10:49:41 -0600

    Base de gráficas por Pokémon individual

--- api.js
+++ api.js
@@ -49,56 +49,57 @@ function consumir() {
 }
 
 // Lectura de varios pokémon a la vez
-let URL = "https://pokeapi.co/api/v2/pokemon/";
-for(let i =1; i <= 10; i++){
-    fetch(URL + i)
-    .then((response) => response.json())
-    .then(data => traerPokemon(data))
-}
+//let URL = "https://pokeapi.co/api/v2/pokemon/";
+//for(let i =1; i <= 10; i++){
+//   fetch(URL + i)
+//    .then((response) => response.json())
+//   .then(data => traerPokemon(data))
+//}
 
 // Concatenación de los tipos de los pokémon solicitados al mismo tiempo
-function traerPokemon(pokeT){
-    let types = pokeT.types.map((type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`);
-    types = types.join('');
-    console.log(types) 
-}
+//function traerPokemon(pokeT){
+//    let types = pokeT.types.map((type) => `<p class="${type.type.name} tipo">${type.type.name}</p>`);
+//    types = types.join('');
+//    console.log(types) 
+//}
 
-// Gráfica treemap de los tipos de pokémon
-function graficarTM(tipo, types){
-    var labels = ["Normal", "Fire", "Water", "Grass", "Electric", "Ice", "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dark", "Dragon", "Steel", "Fairy"];
-    var parents = ["Tipo"];
-    var data2 = [
-        {
-        type: "treemap",
-        labels: labels,
-        parents: parents,
-        }
-    ];
-    var layout = {
-        treemapcolorway: ["blue", "red"],
-        plot_bgcolor: 'rgba(0,0,0,0)',
-            paper_bgcolor: 'rgba(0,0,0,0)',
-            margin: {
-                l: 50,
-                r: 50,
-                b: 50,
-                t: 50,
-                pad: 4,
-            }   
-    };     
-Plotly.newPlot('myDiv', data2);
-}
+// Gráfica treemap, a editar, de los tipos de pokémon
+//function graficarTM(tipo, types){   //Cambiar parámetros
+//    var labels = ["Normal", "Fire", "Water", "Grass", "Electric", "Ice", "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dark", "Dragon", "Steel", "Fairy"];
+//    var parents = ["Tipo"];
+//    var data2 = [
+//        {
+//        type: "treemap",
+//        labels: labels,
+//        parents: parents,
+//        }
+//    ];
+//    var layout = {
+//        treemapcolorway: ["blue", "red"],
+//       plot_bgcolor: 'rgba(0,0,0,0)',
+//            paper_bgcolor: 'rgba(0,0,0,0)',
+//            margin: {
+//                l: 50,
+//                r: 50,
+//                b: 50,
+//                t: 50,
+//                pad: 4,
+//            }   
+//    };     
+//Plotly.newPlot('myDiv', data2);
+//}
 
 // Gráfica de burbuja con experiencia base (mínimo: 20 puntos de experiencia, máximo: 255 o 608 según la generación)
-function graficarB(){
+function graficarBurbuja(){
     var trace1 = {
-        x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-        y: [20, 100, 300, 400, 500],
+        x: [],
+        y: [],
         mode: 'markers',
         marker: {
-            color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-            opacity: [1, 0.8, 0.6, 0.4],
-            size: [40, 60, 80, 100]
+            color: 'rgb(93, 164, 214)',
+            opacity: 1,
+            size: 40,
+            symbol: 'diamond',
         }
     };
 
@@ -106,6 +107,12 @@ function graficarB(){
 
     var layout = {
         title: 'Experiencia base',
+        xaxis: {
+            title: 'Número de Pokémon',
+        },
+        yaxis: {
+            title: 'Cantidad de experiencia base',
+        }
         showlegend: false,
         height: 600,
         width: 600
@@ -114,7 +121,54 @@ function graficarB(){
     Plotly.newPlot('myDiv', data3, layout);
 }
 
+// Gráfica de barra vertical con altura del pokémon
+function graficarBarraV(){
+    var data = [
+        {
+        x: ['Cutiefly', 'Tu Pokémon', 'Eternamax Eternatus'],
+        y: [0.1, , 100],
+        type: 'bar'
+        }
+    ];
+    var layout = {
+        title: 'Altura',
+        xaxis: {
+            title: 'Pokémon',
+        },
+        yaxis: {
+            title: 'Altura en metros',
+        }
+        showlegend: false,
+        height: 600,
+        width: 600
+    };
+    Plotly.newPlot('myDiv', data);
+}
 
+// Gráfica de barra horizontal con peso del pokémon
+function graficarBarraH(){
+    var data=[
+        {
+            type: 'bar',
+            x: [],
+            y: ['Cutiefly', 'Tu Pokémon', 'Eternamax Eternatus'],
+            orientation: 'h'
+        }
+    ];
+    var layout = {
+        title: 'Peso',
+        xaxis: {
+            title: 'Pokémon',
+        },
+        yaxis: {
+            title: 'Peso en kilogramos',
+        }
+        showlegend: false,
+        height: 600,
+        width: 600
+    };
+    Plotly.newPlot('myDiv', data);
+}
 
 
 const myCollapseEl = document.querySelector('#all-graphics') //para eliminar las instrucciones
